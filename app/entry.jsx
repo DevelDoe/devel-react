fc1d8cfea72b4ded916c4e05805b856649fa1b5b
Andrée Ray <info@andreeray.se>
2017-03-03 22:23:02 +0100

    added folders

--- app/entry.jsx
+++ app/entry.jsx
@@ -4,14 +4,10 @@
 * @Email:  me@andreeray.se
 * @Filename: Entry.jsx
 * @Last modified by:   andreeray
-* @Last modified time: 2017-02-27T03:48:01+01:00
+* @Last modified time: 2017-03-03T22:22:16+01:00
 */
 
 var React = require('react'), ReactDOM = require('react-dom'), {Route,Router,IndexRoute,hashHistory} = require('react-router')
-
-// Load foundation
-require('style!css!foundation-sites/dist/css/foundation.min.css')
-$(document).foundation()
 
 //app css
 require('style!css!sass!styles')
