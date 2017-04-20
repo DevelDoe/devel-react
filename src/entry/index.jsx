7d89ce9b4cf75f86955b2ebc1520e9d29488b976
Andrée Ray <info@andreeray.se>
2017-04-20 02:01:01 +0200

    patch

--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -4,7 +4,7 @@
 * @Email:  me@andreeray.se
 * @Filename: app.jsx
  * @Last modified by:   develdoe
- * @Last modified time: 2017-04-20T01:56:35+02:00
+ * @Last modified time: 2017-04-20T02:00:55+02:00
 */
 
 
@@ -43,7 +43,7 @@ console.log(`
  * @Site                : andreeray.se
  * @Patch               : 0.
  * @Last modified by    : develdoe
- * @Last modified time: 2017-04-20T01:56:35+02:00
+ * @Last modified time: 2017-04-20T02:00:55+02:00
 ##############################################
 
 %cABOUT#######################################
@@ -55,53 +55,48 @@ JavaScript ninja @ Devel Devils.
 
 %cRELEASELOG#####################################
 
-0.0 Inital (Framework) branch
-    * 0.1 Install framework modules:
-        * webpack
-        * react
-        * react-dom
-        * express
-        * babel-core
-        * babel-loader
-        * babel-preset-es2015
-        * babel-preset-react
-        * babel-preset-stage-0
-    * 0.2 Setup: Webpack
-    * 0.2 Add: public HTML
-    * 0.3 Add: Simple server
-    * 0.4 Install Test suite modules:
-        * karma
-        * karma-webpack
-        * karma-mocha
-        * karma-mocha-reporter
-        * karma-chrome-launcher
-        * karma-sourcemap-loader
-        * mocha
-        * expect
-        * react-addons-test-utils
-    * 0.4 Add: Entry
-    * 0.4 Test: Entry
-    * 0.5 Add: Component
-    * 0.5 Test: Component
-    * 0.6 Add: List
-    * 0.6 Test: List
-    * 0.7 Add: Item
-    * 0.7 Test: Item
+0.0 Major (Framework) branch
+================================
+* 0.1 Install framework modules:
+    * webpack
+    * react
+    * react-dom
+    * express
+    * babel-core
+    * babel-loader
+    * babel-preset-es2015
+    * babel-preset-react
+    * babel-preset-stage-0
+* 0.2 Setup: Webpack
+* 0.3 Add: public HTML
+* 0.4 Add: Simple server
+* 0.5 Install Test suite modules:
+    * karma
+    * karma-webpack
+    * karma-mocha
+    * karma-mocha-reporter
+    * karma-chrome-launcher
+    * karma-sourcemap-loader
+    * mocha
+    * expect
+    * react-addons-test-utils
+* 0.6 Add: Entry
+* 0.7 Add: Component
+* 0.8 Add: List
+* 0.9 Add: Item
 
 1.0 Major (Redux) branch
-    * 1.1 Install Redux modules:
-        * redux
-        * react-redux
-        * redux-thunk
-        * axios
-        * redux-mock-store
-    * 1.2 Add: Store
-    * 1.2 Test: Store
-    * 1.3 Add: Actions
-    * 1.3 Test: Actions
-    * 1.4 Add: Reducers
-    * 1.4 Test: Reduers
-    * 1.5 Update: component
+================================
+* 1.1 Install Redux modules:
+    * redux
+    * react-redux
+    * redux-thunk
+    * axios
+    * redux-mock-store
+* 1.2 Add: Store
+* 1.3 Add: Actions
+* 1.4 Add: Reducers
+* 1.5 Update: component
 
 ##############################################
 
